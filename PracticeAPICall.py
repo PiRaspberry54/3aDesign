bfd3908aa98c599b00e858b3e1531abf4b6b6f14
Jasmine <Jasmine@Jasmines-MacBook-Pro.local>
2024-06-24 13:55:47 +0100

    Fixed error with invalid and valid product number

--- PracticeAPICall.py
+++ PracticeAPICall.py
@@ -20,12 +20,12 @@ def get_access_token(client_id, client_secret):
     }
     #Post command used to make request to authentication server
     response = requests.post(token_url, headers=headers, data=data)
-    response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+    #response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
     token_info = response.json()
     return token_info['access_token']
 
 # URL to make the API call taken from documentation (needs to be updated using information from )
-api_url = 'https://api.digikey.com/products/v4/search/nf /productdetails'
+api_url = 'https://api.digikey.com/products/v4/search/CL21A106KAYNNNE/productdetails'
 #api_url_DigiReelPricing = 'https:/api.digikey.com/products/v4/search/TMK105BJ104KV-F/digireelpricing'
 #P5555-ND
 
@@ -43,7 +43,7 @@ def make_api_call(api_url, access_token):
     }
     response = requests.get(api_url, headers=headers)
     #HTTP error if the HTTP request returned an unsuccessful status code
-    response.raise_for_status()
+    #response.raise_for_status()
     return response.json()
 
 # Main function to orchestrate the API call
@@ -55,5 +55,17 @@ def main():
     # Calling make api call function to retrieve information from api on product information
     api_response = make_api_call(api_url, access_token)
     print('API Response:', api_response)
-
+    #Checking to see if status has been returned in json data. Indicating product hasn't been found
+    if 'status' in api_response:
+        #404 error indicates no product has been found
+        if api_response['status'] == 404:
+            print("working")
+        #Different error being produced and product still hasn't been found.
+        else:
+            print("Status works")
+    #No status and therefore no error indicating that everything is working and therefore a product has been found.
+    else:
+        unit_price = api_response.get('Product', {}).get('UnitPrice')
+        print("Doesn't work")
+        print("Unit price: ",unit_price)
 main()
