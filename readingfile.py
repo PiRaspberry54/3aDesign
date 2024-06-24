19765f9c83243ddc1660525f1194f61c8e9b4b61
Jasmine <Jasmine@Jasmines-MacBook-Pro.local>
2024-06-24 22:30:03 +0100

    Updating each element to be complete so that they are ready to be joined into one application

--- readingfile.py
+++ readingfile.py
@@ -1,15 +1,36 @@
 import tkinter as tk
 from tkinter import filedialog
+import csv
  
+#text_widget.delete(1.0, tk.END)  # Clear previous content
+#text_widget.insert(tk.END, array_file)
+
 def open_file():
     file_path = filedialog.askopenfilename(
         title="Select a Text File", filetypes=[("Text files", "*.csv")])
     if file_path:
         with open(file_path, 'r') as file:
-            content = file.read()
+            csv_reader = csv.reader(file)
+            array_file = list(csv_reader)
+            array_file_length = len(array_file)
             text_widget.delete(1.0, tk.END)  # Clear previous content
-            text_widget.insert(tk.END, content)
- 
+
+            product_code = []
+            quantity = []
+
+            x = 1
+
+            while array_file_length > x:
+                current_line = array_file[x]
+                product_code_value = current_line[4]
+                quantity_value = current_line[1]
+                product_code.append(product_code_value)
+                quantity.append(quantity_value)
+                x = x+1
+            
+            print(f"Total rows: {array_file_length}")
+            print(f"Product Codes: {product_code}")
+            print(f"Quantities: {quantity}")
  
 # Create the main window
 root = tk.Tk()
