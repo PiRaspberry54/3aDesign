d7df5a166847c488d7740ac8b1a42d157b6807ca
Jasmine <Jasmine@Jasmines-MacBook-Pro.local>
2024-06-25 08:49:58 +0100

    Adding comments to code

--- readingfile.py
+++ readingfile.py
@@ -10,16 +10,21 @@ def open_file():
         title="Select a Text File", filetypes=[("Text files", "*.csv")])
     if file_path:
         with open(file_path, 'r') as file:
+            #Uses CSV library to correctly read the csv file
             csv_reader = csv.reader(file)
+            #Place each line of the file into a list
             array_file = list(csv_reader)
+            #Used to discover length of the file to be used for while loop to go through each line
             array_file_length = len(array_file)
             text_widget.delete(1.0, tk.END)  # Clear previous content
 
             product_code = []
             quantity = []
-
+        
+            #By starting at one you can remove the first line which is headers
             x = 1
 
+            #Goes through each line of the file and looks for the two columns and places them into a variable to be used for api call
             while array_file_length > x:
                 current_line = array_file[x]
                 product_code_value = current_line[4]
